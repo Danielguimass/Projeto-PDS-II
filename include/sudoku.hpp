8aae1ef3440d77ba5e305cd51158bc5f5bc72864
Diogo Nascimento <diogo.nascimento0027@gmail.com>
2024-04-17 22:46:38 -0300

    Implementação inicial do sudoku.

--- include/sudoku.hpp
+++ include/sudoku.hpp
@@ -40,7 +40,7 @@ struct MATRIZ_JOGO{
 
     //métodos:
     void criaMatriz(int dificuldade);
-        //
+        //dependendo da dificuldade, inicia uma das matrizes contidas no /src.
     void imprime();
         //varre linhas e colunas imprimindo na tela o valor da célula se visivel = true, caso contrário, imprime " ".
         //ao varrer por todas as celulas, se nenhuma tiver visivel = false, o jogo termina como vitória.
@@ -49,47 +49,3 @@ struct MATRIZ_JOGO{
         //se errou, tira uma vida e verifica se vidas = 0, se sim, o jogo termina como derrota.
 };
 
-
-
-
-
-
-
-
-
-
-/*
-struct MATRIZES_GERAIS{
-    //atributos: (todos serão matrizes pre definidas, que podemos pegar na internet)
-    //m1:
-    vector<vector<CELULA*>> m1 {
-        {new CELULA(5,true), new CELULA(6,false), new CELULA(8,false), new CELULA(7,false), new CELULA(2,true), new CELULA(4,false), new CELULA(1,false), new CELULA(3,true) , new CELULA(9,true)},
-        {new CELULA(3,false), new CELULA(4,true), new CELULA(2,true), new CELULA(5,false), new CELULA(1,true), new CELULA(9,false), new CELULA(8,false), new CELULA(7,false), new CELULA(6,false)},
-        {new CELULA(1,true), new CELULA(9,false), new CELULA(7,true), new CELULA(3,true), new CELULA(8,false), new CELULA(6,true), new CELULA(5,true), new CELULA(4,false), new CELULA(2,false)},
-        {new CELULA(7,true), new CELULA(3,false), new CELULA(4,false), new CELULA(8,true), new CELULA(9,true), new CELULA(5,true), new CELULA(6,false), new CELULA(2,true), new CELULA(1,true)},
-        {new CELULA(6,true), new CELULA(8,false), new CELULA(5,false), new CELULA(2,true), new CELULA(3,false), new CELULA(1,false), new CELULA(7,true), new CELULA(9,true), new CELULA(4,true)},
-        {new CELULA(2,true), new CELULA(1,true), new CELULA(9,false), new CELULA(4,true), new CELULA(6,true), new CELULA(7,false), new CELULA(3,false), new CELULA(8,false), new CELULA(5,true)},
-        {new CELULA(9,false), new CELULA(2,false), new CELULA(6,false), new CELULA(1,true), new CELULA(7,false), new CELULA(8,true), new CELULA(4,false), new CELULA(5,false), new CELULA(3,true)},
-        {new CELULA(8,false), new CELULA(5,false), new CELULA(1,true), new CELULA(9,false), new CELULA(4,true), new CELULA(3,false), new CELULA(2,true), new CELULA(6,true), new CELULA(7,false)},
-        {new CELULA(4,false), new CELULA(7,false), new CELULA(3,true), new CELULA(6,false), new CELULA(5,false), new CELULA(2,false), new CELULA(9,true), new CELULA(1,false), new CELULA(8,true)},
-    };
-    //m2:
-    vector<vector<CELULA*>> m2 {
-        {new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false) , new CELULA(0,false)},
-        {new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false) , new CELULA(0,false)},
-        {new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false) , new CELULA(0,false)},
-        {new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false) , new CELULA(0,false)},
-        {new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false) , new CELULA(0,false)},
-        {new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false) , new CELULA(0,false)},
-        {new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false) , new CELULA(0,false)},
-        {new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false) , new CELULA(0,false)},
-        {new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false), new CELULA(0,false) , new CELULA(0,false)},
-    };
-    //m3:
-    //...
-
-    //métodos:
-    vector<vector<CELULA*>> escolheMatriz(int dificuldade);
-    //se dificuldade = 1, escolhe uma matriz aleatória de m1 a m5, se dificuldade = 2, uma matriz aleatória de m6 a m10, ...
-};
-*/
